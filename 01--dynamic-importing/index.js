97994de1b2b6dfca255ec955b356afd38cce679f
Roy Vanegas <roy@thecodeeducators.com>
2023-04-11 21:42:10 -0400

    Add dynamic importing example

---
+++ 01--dynamic-importing/index.js
@@ -0,0 +1,11 @@
+import {sayMyName} from "./name.js";
+
+/*
+  • Note that index.js is importing name.js.
+  • Imports — or dynamic imports — must appear at the top.
+  • Use the relative module specifier, “./”, to import modules.
+  • The function “sayMyName()” is available for use locally.
+  • You need a host to use modules. (Loading index.html in your browser won’t work.)
+ */
+
+console.log(`${sayMyName()} is from which TV show?`);
---
+++ 01--dynamic-importing/name.js
@@ -0,0 +1,7 @@
+// The following two variables are only available locally. Prepend “export” to make the variable avaiable outside this file
+let firstName = "Walter";
+let lastName = "White";
+
+export function sayMyName () {
+    return `${firstName} ${lastName}`
+}
