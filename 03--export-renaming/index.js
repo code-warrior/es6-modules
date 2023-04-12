e13df6ef44af5bdb00d0ab0f0dff92bebcd0d300
Roy Vanegas <roy@thecodeeducators.com>
2023-04-12 13:16:17 -0400

    Update comments

--- 03--export-renaming/index.js
+++ 03--export-renaming/index.js
@@ -1,6 +1,12 @@
-/* Import the renamed function, or */
+/* Import the renamed function. */
 import { sayLoudly } from "./renamed-exported-function.js";
+
+// Or rename locally
+// import { sayLoudly as shoutLoudly } from "./renamed-exported-function.js";
+
 // import { shout, sayLoudly } from "./renamed-exported-function.js";
 
 console.log(sayLoudly());
+// console.log(shoutLoudly());
+
 // console.log(shout());
