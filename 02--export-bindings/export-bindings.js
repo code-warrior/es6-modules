b2773d6f9ff781ba5f51fac1dba03d127a822ac8
Roy Vanegas <roy@thecodeeducators.com>
2023-04-12 11:06:58 -0400

    Add export bindings example

---
+++ 02--export-bindings/export-bindings.js
@@ -0,0 +1,5 @@
+// let occupation = `high school teacher`;
+// let moonlighting = `meth dealer`;
+
+export let occupation = `high school teacher`;
+export let moonlighting = `meth dealer`;
---
+++ 02--export-bindings/index.js
@@ -0,0 +1,6 @@
+/* “occupation” and “moonlighting” are read-only and act as constants */
+import { occupation, moonlighting } from "./export-bindings.js"
+// import { sayMyName } from "../01--dynamic-importing/name.js";
+
+// console.log(`${sayMyName()} works as a ${occupation} during the day and as a ${moonlighting} at night`);
+console.log(`Some people work as a ${occupation} during the day and as a ${moonlighting} at night`);
